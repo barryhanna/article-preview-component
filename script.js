4c35fd26b80ba3e7d7f50876f828495dab12c408
Barry Hanna <barryahanna@gmail.com>
2021-06-11 21:12:03 +0100

    Cleaned up mobile design

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-const shareBtn = document.getElementById('toggle-share');
-const shareMenu = document.getElementById('share');
+const shareBtn = document.getElementById('toggle-share-btn');
+const shareMenu = document.getElementById('share-menu');
 const shareMenuBtn = document.getElementById('share-menu-btn');
 console.log(shareBtn);
 
 shareBtn.addEventListener('click', (e) => {
-  const xPos = e.clientX;
-  const viewportWidth = window.innerWidth;
-  const right = viewportWidth - xPos;
-  console.log(`Right: ${right}`);
-  console.log(xPos);
-  shareMenu.style.right = `${right}px`;
   shareMenu.classList.toggle('show');
-  console.log(`Share Menu Left: ${shareMenu.style.right}`);
 });
 
 shareMenuBtn.addEventListener('click', (e) => {
