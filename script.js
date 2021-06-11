531fd6184e6a4dd7def8fcf5d02014f1de248ade
Barry Hanna <barryahanna@gmail.com>
2021-06-11 11:22:50 +0100

    Reverted back to using JavaScript for share menu

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const shareBtn = document.getElementById('toggle-share');
 const shareMenu = document.getElementById('share');
+const shareMenuBtn = document.getElementById('share-menu-btn');
 console.log(shareBtn);
 
 shareBtn.addEventListener('click', (e) => {
   const xPos = e.clientX;
+  const viewportWidth = window.innerWidth;
+  const right = viewportWidth - xPos;
+  console.log(`Right: ${right}`);
   console.log(xPos);
-  shareMenu.style.left = `${xPos}px`;
-  console.log(`Share Menu Left: ${shareMenu.style.left}`);
+  shareMenu.style.right = `${right}px`;
+  shareMenu.classList.toggle('show');
+  console.log(`Share Menu Left: ${shareMenu.style.right}`);
+});
+
+shareMenuBtn.addEventListener('click', (e) => {
+  shareMenu.classList.toggle('show');
 });
