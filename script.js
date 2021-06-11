a050bd8ce17a1070a2e94bf78b4b8365e926e943
Barry Hanna <barryahanna@gmail.com>
2021-06-11 22:17:55 +0100

    Updated desktop design

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const shareBtn = document.getElementById('toggle-share-btn');
 const shareMenu = document.getElementById('share-menu');
 const shareMenuBtn = document.getElementById('share-menu-btn');
-console.log(shareBtn);
 
 shareBtn.addEventListener('click', (e) => {
+  shareMenu.clientRight = e.clientX - shareMenu.style.width / 2;
+  console.log(shareMenu.clientRight);
   shareMenu.classList.toggle('show');
 });
 
 shareMenuBtn.addEventListener('click', (e) => {
   shareMenu.classList.toggle('show');
 });
+
+function moveShareMenu() {
+  const article = shareMenu.closest('article');
+  const footer = article.querySelector('.article-footer');
+  if (window.innerWidth > 599) {
+    article.appendChild(shareMenu);
+  } else {
+    footer.appendChild(shareMenu);
+  }
+}
+
+window.onresize = moveShareMenu;
